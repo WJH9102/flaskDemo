09b3c296d87d8d9c1f7fa87ec62f3d7731ab6aee
wjh <1602526012@qq.com>
2019-07-01 14:26:28 +0800

    使用pycharm提交到Git

---
+++ demo/Hello.py
@@ -0,0 +1,34 @@
+import random
+from flask import Flask, request, render_template, redirect
+import qrcode
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/qr')
+def qrcode1():
+    return render_template("qrcode.html")
+
+
+@app.route('/qrtool', methods=['GET', 'POST'])
+def qrcode_tool():
+    file_list = os.listdir("./static")
+    for file in file_list:
+        path_file = os.path.join("./static", file)
+        if os.path.isfile(path_file):
+            os.remove(path_file)
+    path = random.randint(0, 10000)
+    content = request.args['content']
+    img = qrcode.make(content)
+    img.save("static/{}.jpg".format(path))
+    return redirect("/static/{}.jpg".format(path))
+
+
+if __name__ == '__main__':
+    app.run(port=8880, debug=True)
